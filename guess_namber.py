8b1a9e422972aae080faef438960826bae4e14dd
Viacheslav Borisov <borisov.slava00@yandex.ru>
2023-12-30 00:14:47 +0400

    n

---
+++ guess_namber.py
@@ -0,0 +1,20 @@
+import random
+
+number = random.randint(1,100)
+
+
+print('Угадай число от 1 до 100')
+while True:
+    guess=int(input('Введите число: '))
+
+    if guess < number:
+        print('Ваше число меньше того, что загадано')
+    elif guess > number:
+        print('Ваше число больше, того, что загадано')
+    elif guess == number:
+        print('Отличная интуиция! Вы угадали число :)')
+        False
+    else:
+        print('Введите правильное число!')
+
+        
