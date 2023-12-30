fb45d6a2b69d55c5d0ea96dbd763ac4aff597ac1
Viacheslav Borisov <borisov.slava00@yandex.ru>
2023-12-30 11:35:37 +0400

    optimize key logic

--- guess_namber.py
+++ guess_namber.py
@@ -4,17 +4,19 @@ number = random.randint(1,100)
 
 
 print('Угадай число от 1 до 100')
+
 while True:
     guess=int(input('Введите число: '))
-
-    if guess < number:
-        print('Ваше число меньше того, что загадано')
-    elif guess > number:
-        print('Ваше число больше, того, что загадано')
-    elif guess == number:
-        print('Отличная интуиция! Вы угадали число :)')
-        False
+    if 1<=guess<=100:
+        if guess < number:
+            print('Ваше число меньше того, что загадано')
+        elif guess > number:
+            print('Ваше число больше, того, что загадано')
+        elif guess == number:
+            print('Отличная интуиция! Вы угадали число :)')
+            False
     else:
         print('Введите правильное число!')
+        
 
         
